3483bc6978115fbc73a4782d1882ebc64d6dce1c
nitrosrus <nitrosrus@gmail.com>
2019-12-19 00:28:16 +0300

    портирую drawer в свой прока разбираюсь как это работает

--- app/src/main/java/com/example/nitrosrusweatherapp/ActivityWeather.java
+++ app/src/main/java/com/example/nitrosrusweatherapp/ActivityWeather.java
@@ -34,7 +34,7 @@ public class ActivityWeather extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
