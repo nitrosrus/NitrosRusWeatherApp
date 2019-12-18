6489da1783e7af34755ccae2ed03bbcae3e23356
nitrosrus <nitrosrus@gmail.com>
2019-12-19 00:22:23 +0300

    портирую drawer в свой проэкт

--- app/src/main/java/com/example/nitrosrusweatherapp/ActivityWeather.java
+++ app/src/main/java/com/example/nitrosrusweatherapp/ActivityWeather.java
@@ -2,6 +2,12 @@ package com.example.nitrosrusweatherapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -10,8 +16,12 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class ActivityWeather extends AppCompatActivity {
@@ -19,58 +29,116 @@ public class ActivityWeather extends AppCompatActivity {
     private static String WEATHER_KEY_TAG = "123hgfyweubwe1ui2y3b1i2hbk12h4gf";
     private CitySaver citySaver;
 
+    private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
-        citySaver = new CitySaver(this);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(R.id.container_for_fragments, new FragmentWeatherActivity(), WEATHER_KEY_TAG).commit();
-        }
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.weather, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.change_city) {
-            showInputDialog();
-            return true;
-        }
-        return false;
-    }
-
-    private void showInputDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(getString(R.string.change_city_dialog));
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
-        builder.setPositiveButton(BUTTON_KEY_OK, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                changeCity(input.getText().toString());
-
-            }
-        });
-        builder.show();
-    }
-
-    private void changeCity(String city) {
-        FragmentWeatherActivity fragmentWeatherActivity = (FragmentWeatherActivity) getSupportFragmentManager().findFragmentByTag(WEATHER_KEY_TAG);
-        fragmentWeatherActivity.changeCity(city);
-            citySaver.setCity(city);
-
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_weather);
+//        citySaver = new CitySaver(this);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().add(R.id.container_for_fragments, new FragmentWeatherActivity(), WEATHER_KEY_TAG).commit();
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.weather, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if (item.getItemId() == R.id.change_city) {
+//            showInputDialog();
+//            return true;
+//        }
+//        return false;
+//    }
+
+//    private void showInputDialog() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle(getString(R.string.change_city_dialog));
+//        final EditText input = new EditText(this);
+//        input.setInputType(InputType.TYPE_CLASS_TEXT);
+//        builder.setView(input);
+//        builder.setPositiveButton(BUTTON_KEY_OK, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                changeCity(input.getText().toString());
+//
+//            }
+//        });
+//        builder.show();
+//    }
+//
+//    private void changeCity(String city) {
+//        FragmentWeatherActivity fragmentWeatherActivity = (FragmentWeatherActivity) getSupportFragmentManager().findFragmentByTag(WEATHER_KEY_TAG);
+//        fragmentWeatherActivity.changeCity(city);
+//            citySaver.setCity(city);
+//
+//    }
+//
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//    }
 }
