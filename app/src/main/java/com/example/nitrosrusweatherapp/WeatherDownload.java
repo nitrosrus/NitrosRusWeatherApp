eb92d29d8e037ec27e4f626f98287a954367d4e2
nitrosrus <nitrosrus@gmail.com>
2020-01-12 13:18:20 +0300

    вообще перестало работать

--- app/src/main/java/com/example/nitrosrusweatherapp/WeatherDownload.java
+++ app/src/main/java/com/example/nitrosrusweatherapp/WeatherDownload.java
@@ -28,7 +28,6 @@ public class WeatherDownload {
     private static final String KEY = "94bde3146fcb9c9591279a0cff298631";
     private static OpenWeather openWeather;
     private Retrofit retrofit;
-    private FragmentWeatherActivity fragmentWeatherActivity;
     private static WeatherDownload instance = null;
     private Timer timer = null;
     Handler handler = new Handler();
@@ -43,12 +42,11 @@ public class WeatherDownload {
         listeners = new HashSet<>();
         retrofit = new Retrofit.Builder().baseUrl("http://api.openweathermap.org")
                 .addConverterFactory(GsonConverterFactory.create())
-                .addConverterFactory(ScalarsConverterFactory.create()).build();
+               .addConverterFactory(ScalarsConverterFactory.create()).build();
         openWeather = retrofit.create(OpenWeather.class);
         updateData();
 
     }
-
     public interface OpenWeather {
         @GET("data/2.5/weather")
         Call<WeatherModel> getWeather(@Query("q") String q, @Query("appid") String keyApi);
@@ -56,16 +54,18 @@ public class WeatherDownload {
 
     private static WeatherModel responseRetrofit(String city) throws Exception {
 
-        Call<WeatherModel> call = openWeather.getWeather(city + ",ru", KEY);
+        Call<WeatherModel> call = openWeather.getWeather(city + ",ru","94bde3146fcb9c9591279a0cff298631");
         Response<WeatherModel> response = call.execute();
 
         if (response.isSuccessful())
             return response.body();
 
         else
-            throw new Exception(response.body().getName(), null);
+            throw new Exception(response.errorBody().string(), null);
 
     }
+
+
 
     public void updateData() {
 
@@ -82,7 +82,7 @@ public class WeatherDownload {
                         @Override
                         public void run() {
                             for (WeatherDownloadListener listener : listeners) {
-                                fragmentWeatherActivity.updateWeather(model);
+                                listener.updateWeather(model);
 
                             }
                         }
